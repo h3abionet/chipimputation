bb898e872f2b08546157dfe8226f0d9b46cbdca5
Mamana <mypandos@gmail.com>
2018-10-02 14:13:13 +0200

    Fixes

--- templates/report_well_imputed.py
+++ templates/report_well_imputed.py
@@ -9,7 +9,7 @@ parser.add_argument("--inWell_imputed", help="")
 args = parser.parse_args()
 
 
-def well_imputed_by_maf(inWell_imputed, outWell_imputed):
+def well_imputed_by_maf(inWell_imputed, outWell_imputed, dataset=''):
     """
 
     :return:
@@ -18,17 +18,21 @@ def well_imputed_by_maf(inWell_imputed, outWell_imputed):
     outWell_imputed_out = open(outWell_imputed, 'w')
     outWell_imputed_out_1 = open(outWell_imputed + "_summary.tsv", 'w')
     outWell_imputed_out_1.writelines('\\t'.join(['CHRM', 'MAF<1%', 'MAF 1-5%', 'MAF>5%', 'TOTAL']) + '\\n')
-    outWell_imputed_out.writelines('\\t'.join(['CHRM', 'MAF<1%', 'MAF 1-5%', 'MAF>5%']) + '\\n')
+    outWell_imputed_out.writelines('\\t'.join(['DATASET', 'MAF<1%', 'MAF 1-5%', 'MAF>5%']) + '\\n')
     info_datas = open(inWell_imputed).readlines()
+    total = {}
+    total['rare'] = []
+    total['moderate'] = []
+    total['common'] = []
     for line in info_datas:
         data = line.strip().split()
-        dataset = data[0]
-        if dataset not in datas:
-            datas[dataset] = {}
-            datas[dataset]['rare'] = []
-            datas[dataset]['moderate'] = []
-            datas[dataset]['common'] = []
-            datas[dataset]['total'] = 0
+        chrm = data[0]
+        if chrm not in datas:
+            datas[chrm] = {}
+            datas[chrm]['rare'] = []
+            datas[chrm]['moderate'] = []
+            datas[chrm]['common'] = []
+            datas[chrm]['total'] = 0
         if "SNP" in line and "Rsq" in line:
             idx_exp_freq_a1 = data.index('MAF')
         else:
@@ -36,23 +40,28 @@ def well_imputed_by_maf(inWell_imputed, outWell_imputed):
             if maf >= 0.5:
                 maf = 1 - maf
             if maf < 0.01:
-                datas[dataset]['rare'].append(maf)
-                datas[dataset]['total'] += 1
+                datas[chrm]['rare'].append(maf)
+                total['rare'].append(maf)
+                datas[chrm]['total'] += 1
             elif maf > 0.05:
-                datas[dataset]['common'].append(maf)
-                datas[dataset]['total'] += 1
+                datas[chrm]['common'].append(maf)
+                total['common'].append(maf)
+                datas[chrm]['total'] += 1
             elif maf <= 0.05 and maf >= 0.01:
-                datas[dataset]['moderate'].append(maf)
-                datas[dataset]['total'] += 1
+                datas[chrm]['moderate'].append(maf)
+                total['moderate'].append(maf)
+                datas[chrm]['total'] += 1
     try:
         datasets = [str(it) for it in sorted([int(it) for it in datas])]
     except:
         datasets = sorted(datas)
-    for dataset in datasets:
-        tot = datas[dataset]['total']
-        rare = len(datas[dataset]['rare'])
-        moderate = len(datas[dataset]['moderate'])
-        common = len(datas[dataset]['common'])
+
+    # Write per chromosome or pop
+    for chrm in datasets:
+        tot = datas[chrm]['total']
+        rare = len(datas[chrm]['rare'])
+        moderate = len(datas[chrm]['moderate'])
+        common = len(datas[chrm]['common'])
         if rare >= 1000000:
             rare_ = str(format(rare / 1000000., '0,.1f')) + 'M ('
         else:
@@ -66,23 +75,25 @@ def well_imputed_by_maf(inWell_imputed, outWell_imputed):
         else:
             common_ = str(format(common, '0,')) + ' ('
         if tot == 0:
-            outWell_imputed_out.write("dataset {} is empty (tot=0)".format(dataset))
-            outWell_imputed_out_1.write("dataset {} is empty (tot=0)".format(dataset))
+            outWell_imputed_out.write("dataset {} is empty (tot=0)".format(chrm))
+            outWell_imputed_out_1.write("dataset {} is empty (tot=0)".format(chrm))
         else:
-            outWell_imputed_out.writelines('\\t'.join([dataset, \
-                                                       str(len(datas[dataset]['rare'])), \
-                                                       str(len(datas[dataset]['moderate'])), \
-                                                       str(len(datas[dataset]['common']))]) + '\\n')
-            outWell_imputed_out_1.writelines('\\t'.join([dataset, \
+            outWell_imputed_out_1.writelines('\\t'.join([chrm, \
                                                          rare_ + str(rare * 100 / tot) + '%)', \
-                                                       moderate_ + str(moderate * 100 / tot) + '%)', \
-                                                       common_ + str(common * 100 / tot) + '%)', \
-                                                       str(format(tot / 1000000., '0,.1f')) + 'M']) + '\\n')
+                                                         moderate_ + str(moderate * 100 / tot) + '%)', \
+                                                         common_ + str(common * 100 / tot) + '%)', \
+                                                         str(format(tot / 1000000., '0,.1f')) + 'M']) + '\\n')
+    # Write the overall for dataset
+    outWell_imputed_out.writelines('\\t'.join([dataset, \
+                                               str(len(total['rare'])), \
+                                               str(len(total['moderate'])), \
+                                               str(len(total['common']))]) + '\\n')
     outWell_imputed_out_1.close()
     outWell_imputed_out.close()
 
 
 args.inWell_imputed = "${inWell_imputed}"
 args.outWell_imputed = "${outWell_imputed}"
+args.dataset = "${target_name}"
 if args.inWell_imputed and args.outWell_imputed:
-    well_imputed_by_maf(args.inWell_imputed, args.outWell_imputed)
+    well_imputed_by_maf(args.inWell_imputed, args.outWell_imputed, args.dataset)
